a9e1965a669726a4f03332e33470877b70324ed2
beingpramudita <pramudita644@gmail.com>
2022-04-06 08:17:23 +0545

    Added Task-2

---
+++ README.md
@@ -0,0 +1,6 @@
+## Task 2 
+
+***Added Scrollview and active web link***
+<br>
+<br>
+![scrollingText-task2](https://user-images.githubusercontent.com/47735236/111351500-5647ae80-86ab-11eb-9c10-e23cfb624bb4.gif)
---
+++ app/src/main/java/com/pramudita/scrollingtext/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pramudita.scrollingtext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
